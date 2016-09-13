20de352c85e7861363d64d13c092efa84fd96b63
gspinnadauhu <markusalang@gmail.com>
2016-09-13 17:37:02 -0500

    splitting off part 2 into best.R

---
+++ best.R
@@ -0,0 +1,10 @@
+#Programming Assignment3
+##2 Finding the best hospital in a state
+best <- function(state,outcome){
+        ###Read outcome data
+        
+        ###Check that state and outcome are valid
+        
+        ###Return hispital name in that state with lowerst 30day death rate
+        
+}
