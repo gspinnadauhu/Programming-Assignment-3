cc8e0fec8c12fc3bd690f1af97f74ec34f190425
gspinnadauhu <markusalang@gmail.com>
2016-09-13 18:34:03 -0500

    setting up if else to subset correct columns

--- best.R
+++ best.R
@@ -2,9 +2,18 @@
 ##2 Finding the best hospital in a state
 best <- function(state,outcome){
         ###Read outcome data
-        
+        datafile <- read.csv("outcome-of-care-measures.csv")
+        ###heartattack-col 11, heartfail-col 17, pneumonia-col 23
+        if(outcome="heart attack"){
+                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,11)])   
+        }else if(outcome="heart failure"){
+                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,17)])
+        }else if(outcome="pneumonia"){
+                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,23)])
+        }else{
+                print("Error: please choose from 'heart attack', 'heart failure' or 'pneumonia'")
+        }
         ###Check that state and outcome are valid
         
         ###Return hispital name in that state with lowerst 30day death rate
-        
 }
