6e6ca18294119f4aa0bd8622ef205b5a528ad0f6
gspinnadauhu <markusalang@gmail.com>
2016-09-13 20:17:12 -0500

    fixed argument validation part & error message

--- best.R
+++ best.R
@@ -5,23 +5,20 @@ best <- function(state,outcome){
         datafile <- read.csv("outcome-of-care-measures.csv")
         ###Check that state and outcome are valid
         types <- c("heart attack","heart failure","pneumonia")
-        if(any(state!=datafile$State)){
-                paste("Error in best(",state,outcome,"): invalid state")
-        }
-        if(any(outcome!=types)){
-                paste("Error in best(",state,outcome,"): invalid outcome")
-        }
-        ###Return hispital name in that state with lowerst 30day death rate
-        
-        ###heartattack-col 11, heartfail-col 17, pneumonia-col 23
-        if(outcome="heart attack"){
-                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,11)])   
-        }else if(outcome="heart failure"){
-                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,17)])
-        }else if(outcome="pneumonia"){
-                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,23)])
+        if(!state %in% datafile[,7]){
+                stop('invalid state')
+        }else if(!outcome %in% types){
+                stop('invalid outcome')
         }else{
-                print("Error: please choose from 'heart attack', 'heart failure' or 'pneumonia'")
+        ###Return hispital name in that state with lowerst 30day death rate
+        ####heartattack-col 11, heartfail-col 17, pneumonia-col 23
+                if(outcome=="heart attack"){
+                        rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,11)])   
+                }else if(outcome=="heart failure"){
+                        rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,17)])
+                }else if(outcome=="pneumonia"){
+                rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,23)])
+                }else{}
+                
         }
-        
 }
