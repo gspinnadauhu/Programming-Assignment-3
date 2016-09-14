0d69e60c85ab88636d4b102f4c461cc1ba20d8f5
gspinnadauhu <markusalang@gmail.com>
2016-09-13 19:25:17 -0500

    checking validity of arguments

--- best.R
+++ best.R
@@ -3,6 +3,16 @@
 best <- function(state,outcome){
         ###Read outcome data
         datafile <- read.csv("outcome-of-care-measures.csv")
+        ###Check that state and outcome are valid
+        types <- c("heart attack","heart failure","pneumonia")
+        if(any(state!=datafile$State)){
+                paste("Error in best(",state,outcome,"): invalid state")
+        }
+        if(any(outcome!=types)){
+                paste("Error in best(",state,outcome,"): invalid outcome")
+        }
+        ###Return hispital name in that state with lowerst 30day death rate
+        
         ###heartattack-col 11, heartfail-col 17, pneumonia-col 23
         if(outcome="heart attack"){
                 rates <- subset.data.frame(datafile,State=state,select=datafile[,c(2,11)])   
@@ -13,7 +23,5 @@ best <- function(state,outcome){
         }else{
                 print("Error: please choose from 'heart attack', 'heart failure' or 'pneumonia'")
         }
-        ###Check that state and outcome are valid
         
-        ###Return hispital name in that state with lowerst 30day death rate
 }
