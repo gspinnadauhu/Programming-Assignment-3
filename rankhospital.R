f8c05286e6b2ba100e1a28a1e3e9f565372a74d7
gspinnadauhu <markusalang@gmail.com>
2016-09-15 18:57:16 -0500

    finished and ok per the sample outputs

--- rankhospital.R
+++ rankhospital.R
@@ -16,30 +16,29 @@ rankhospital <- function(state, outcome, num = "best") {
         shortset$heartfailure<-suppressWarnings(as.numeric(shortset$heartfailure))
         shortset$pneumonia<-suppressWarnings(as.numeric(shortset$pneumonia))
         shortset<-subset(shortset,State==state,select=c("Hospital","State","heartattack","heartfailure","pneumonia"))
-        ##ordering by outcome in ascending order
-        attach(shortset)
+        ##ordering by outcome in ascending order, removing non-selected outcomes
         if(outcome=="heart attack"){
                 colnum<-c(3)
-                shortset<-shortset[with(shortset,order(heartattack,Hospital)),c(1,colnum)]  
+                shortset<-shortset[with(shortset,order(shortset$heartattack,shortset$Hospital)),c(1,colnum)]  
         }else if(outcome=="heart failure"){
                 colnum<-c(4)
-                shortset<-shortset[with(shortset,order(heartfailure,Hospital)),c(1,colnum)]
+                shortset<-shortset[with(shortset,order(shortset$heartfailure,shortset$Hospital)),c(1,colnum)]
         }else{
                 colnum<-c(5)
-                shortset<-shortset[with(shortset,order(pneumonia,Hospital)),c(1,colnum)] 
+                shortset<-shortset[with(shortset,order(shortset$pneumonia,shortset$Hospital)),c(1,colnum)] 
         }
         shortset<-na.omit(shortset)
         ranking<-c(1:length(shortset$Hospital))
         shortset<-cbind.data.frame(shortset,ranking)
         names(shortset)<-c("Hospital","Rate","Rank")
-        ## 30-day death rate
-        #if(num=="best"){
-                
-                #shortset[c(1:5),c(1,2,colnum)]    
-        #}else if(num=="worst"){
-        #        length()
-        #        shortset[c(1:5),]        
-        #}
-        shortset[c(1:5),]
-        #shortset
+        ## 30-day death rate - setting up selection based on num=
+        if(num=="best"){
+                shortset[1,]    
+        }else if(num=="worst"){
+                shortset[length(shortset$Hospital)]
+        }else if(num>length(shortset$Hospital) | num<1){
+                return(NA)
+        }else{
+                shortset[num,1]
+        }
 }
