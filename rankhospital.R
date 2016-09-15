6bc91f7d7b28c546db5a95941a1bc44a8ca53a19
gspinnadauhu <markusalang@gmail.com>
2016-09-14 20:41:05 -0500

    copying over the basics of the function from existing function

--- rankhospital.R
+++ rankhospital.R
@@ -1,6 +1,12 @@
 rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
+        datafile <- read.csv("outcome-of-care-measures.csv",stringsAsFactors = FALSE)
         ## Check that state and outcome are valid
+        if(!state %in% datafile[,7]){
+                stop('invalid state')
+        }else if(!outcome %in% types){
+                stop('invalid outcome')
+        }else{}
         ## Return hospital name in that state with the given rank
         ## 30-day death rate
 }
