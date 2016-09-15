7da7d42e16ccde8a3b178073de0cb043bfa32558
gspinnadauhu <markusalang@gmail.com>
2016-09-14 20:49:17 -0500

    setting up like the last function

--- rankhospital.R
+++ rankhospital.R
@@ -2,11 +2,20 @@ rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
         datafile <- read.csv("outcome-of-care-measures.csv",stringsAsFactors = FALSE)
         ## Check that state and outcome are valid
+        types <- c("heart attack","heart failure","pneumonia")
         if(!state %in% datafile[,7]){
                 stop('invalid state')
         }else if(!outcome %in% types){
                 stop('invalid outcome')
         }else{}
         ## Return hospital name in that state with the given rank
+        ## creating a subset dataframe 
+        shortset<-datafile[,c(2,7,11,17,23)]
+        names(shortset)<-c("Hospital","State","heartattack","heartfailure","pneumonia")
+        shortset$heartattack<-suppressWarnings(as.numeric(shortset$heartattack))
+        shortset$heartfailure<-suppressWarnings(as.numeric(shortset$heartfailure))
+        shortset$pneumonia<-suppressWarnings(as.numeric(shortset$pneumonia))
+        shortset<-subset(shortset,State==state,select=c("Hospital","State","heartattack","heartfailure","pneumonia"))
+        
         ## 30-day death rate
 }
