f4393d611c7e647e01060cf41f932c945a9877b2
gspinnadauhu <markusalang@gmail.com>
2016-09-15 19:04:15 -0500

    setting up read and data validation

--- rankall.R
+++ rankall.R
@@ -1,6 +1,11 @@
 rankall <- function(outcome, num = "best") {
         ## Read outcome data
-        ## Check that state and outcome are valid
+        datafile <- read.csv("outcome-of-care-measures.csv",stringsAsFactors = FALSE)
+        ## Check that outcome is valid
+        types <- c("heart attack","heart failure","pneumonia")
+        if(!outcome %in% types){
+                stop('invalid outcome')
+        }else{}
         ## For each state, find the hospital of the given rank
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
