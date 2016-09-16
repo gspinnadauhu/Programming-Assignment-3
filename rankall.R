75e663dbd6b4c9bd76ad1af0366fc3e122a26392
gspinnadauhu <markusalang@gmail.com>
2016-09-15 22:18:33 -0500

    used same base as for rankhospital function, sorted subsetted shortsets by state first then rate then alphabetically. added colnames, took out NAs, and found this do.call function to grab the nth item in a list per a column

--- rankall.R
+++ rankall.R
@@ -6,7 +6,34 @@ rankall <- function(outcome, num = "best") {
         if(!outcome %in% types){
                 stop('invalid outcome')
         }else{}
+        ## creating a subset dataframe 
+        shortset<-datafile[,c(2,7,11,17,23)]
+        names(shortset)<-c("Hospital","State","heartattack","heartfailure","pneumonia")
+        shortset$heartattack<-suppressWarnings(as.numeric(shortset$heartattack))
+        shortset$heartfailure<-suppressWarnings(as.numeric(shortset$heartfailure))
+        shortset$pneumonia<-suppressWarnings(as.numeric(shortset$pneumonia))
+        if(outcome=="heart attack"){
+                colnum<-c(3)
+                shortset<-shortset[with(shortset,order(shortset$State,
+                                                       shortset$heartattack,
+                                                       shortset$Hospital)),c(1,2,colnum)]  
+        }else if(outcome=="heart failure"){
+                colnum<-c(4)
+                shortset<-shortset[with(shortset,order(shortset$State,
+                                                       shortset$heartfailure,
+                                                       shortset$Hospital)),c(1,2,colnum)] 
+        }else{
+                colnum<-c(5)
+                shortset<-shortset[with(shortset,order(shortset$State,
+                                                       shortset$pneumonia,
+                                                       shortset$Hospital)),c(1,2,colnum)] 
+        }
         ## For each state, find the hospital of the given rank
+        names(shortset)<-c("Hospital","State","Rate")
+        shortset<-na.omit(shortset)
+        #ranking<-tapply(shortset$Rate,shortset$State,seq_along)
+        #shortset<-cbind.data.frame(shortset,ranking)
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
+        do.call(rbind,by(shortset,shortset$State,function(x) x[num,]))
 }
